c570849ec6474ea78fda8c8933cf83daa5198987
dberry38 <108836058+dberry38@users.noreply.github.com>
2022-11-23 19:41:05 -0800

    Develop seed data; add content to Story model

--- models/index.js
+++ models/index.js
@@ -1,9 +1,11 @@
 const User = require('./User');
 const Story = require('./Story');
+const User_Stories = sequelize.define('User_stories', {}, { timestamps: false });
 
 User.hasMany(Story, {
+  through: 'User_Stories',
   foreignKey: 'user_id',
-  onDelete: 'NO ACTION'
+  onDelete: 'NO ACTION',
 });
 
 Story.belongsToMany(User, {
