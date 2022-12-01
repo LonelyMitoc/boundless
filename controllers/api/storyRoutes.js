5fb16ef0046ac1f04b2e156034502226c224eabb
dankturtle <abrowncownow@gmail.com>
2022-11-30 18:39:54 -0800

    bad package removed

--- controllers/api/storyRoutes.js
+++ controllers/api/storyRoutes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const { Store } = require('express-session');
 const { Story, User, UserStories } = require('../../models');
 const withAuth = require('../../utils/auth');
 
